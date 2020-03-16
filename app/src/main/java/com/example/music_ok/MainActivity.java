de6cf1d951fe963067a5eb01f23d0894fe44e7e0
kyj <dudwns0911@naver.com>
2020-03-16 20:41:49 +0900

    Second project

---
+++ app/src/main/java/com/example/music_ok/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.music_ok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView =findViewById(R.id.hello);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),AcitivityName.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
