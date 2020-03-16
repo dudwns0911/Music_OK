7aa641f1d0d324843041e45c83d7af3a3882eee1
dudwns0911 <dudwns0911@naver.com>
2020-03-16 22:35:44 +0900

    music finish

--- MUSIC_OK/app/src/main/java/com/example/music_ok/MainActivity.java
+++ MUSIC_OK/app/src/main/java/com/example/music_ok/MainActivity.java
@@ -3,24 +3,48 @@ package com.example.music_ok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    int length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView =findViewById(R.id.hello);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(),AcitivityName.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mediaPlayer =MediaPlayer.create(this, R.raw.ground);
+        mediaPlayer.setLooping(false);
+        mediaPlayer.seekTo(length);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer.isPlaying()){
+            length = mediaPlayer.getCurrentPosition();
+            mediaPlayer.pause();
+            mediaPlayer.release();
+    }
+}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            length =mediaPlayer.getCurrentPosition();
+            mediaPlayer.release();
+        }
     }
 }
