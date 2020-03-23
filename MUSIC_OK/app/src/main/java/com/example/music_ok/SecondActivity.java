97cc9ae283b147e9020015f730c368611bbf9472
dudwns0911 <dudwns0911@naver.com>
2020-03-23 21:10:12 +0900

    videoview

--- MUSIC_OK/app/src/main/java/com/example/music_ok/SecondActivity.java
+++ MUSIC_OK/app/src/main/java/com/example/music_ok/SecondActivity.java
@@ -2,14 +2,29 @@ package com.example.music_ok;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        Log.d("this", "test");
+
+        VideoView videoView = (VideoView) findViewById(R.id.video);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.room);
+        MediaController myController = new MediaController(this);
+        myController.setAnchorView(videoView);
+        videoView.setMediaController(myController);
+        videoView.start();
     }
+
+
 }
