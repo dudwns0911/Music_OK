2e7211a13e6a5fd1e3de8ddc188cfd6828d58615
dudwns0911 <dudwns0911@naver.com>
2020-03-23 20:06:51 +0900

    videoview

--- MUSIC_OK/app/src/main/java/com/example/music_ok/MainActivity.java
+++ MUSIC_OK/app/src/main/java/com/example/music_ok/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer = new MediaPlayer();
@@ -16,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView mimageView = findViewById(R.id.lol);
+        mimageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "이미지를 클릭하셨습니다.", Toast.LENGTH_SHORT).show();
+                final Intent intent = new Intent(v.getContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -40,11 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
+            super.onDestroy();
         if(mediaPlayer.isPlaying()){
             mediaPlayer.pause();
             length =mediaPlayer.getCurrentPosition();
             mediaPlayer.release();
         }
     }
+
+
 }
